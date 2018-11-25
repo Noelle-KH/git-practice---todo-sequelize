ab0cc23d433eece76aac02b1de65490a520ede47
v123582 <v123582@gmail.com>
2018-11-25 22:57:31 +0800

    fisrt route to get data

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const app = express()
 const port = 3000
 
 const db = require('./models')
+const User = db.User
+const Task = db.Task
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/users', (req, res) =>
+  User.findAll().then((users) => res.json(users))
+)
+
+app.get('/tasks', (req, res) =>
+  Task.findAll().then((tasks) => res.json(tasks))
+)
 
 app.listen(port, () => {
   db.sequelize.sync()
