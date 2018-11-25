b5911dd061f798a767465a689259379491126ab8
v123582 <v123582@gmail.com>
2018-11-25 22:55:00 +0800

    attach sequelize to express

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require('./models')
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () =>
+app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`Example app listening on port ${port}!`)
-)
+})
