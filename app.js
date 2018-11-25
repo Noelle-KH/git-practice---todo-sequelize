70ae0e0713b231e177dc6bb9477fff8ff7afc68c
v123582 <v123582@gmail.com>
2018-11-25 23:02:26 +0800

    attach handlebars view

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
@@ -6,7 +7,10 @@ const db = require('./models')
 const User = db.User
 const Task = db.Task
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => res.render('index'))
 
 app.get('/users', (req, res) =>
   User.findAll().then((users) => res.json(users))
