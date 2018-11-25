e341097f3eba8325774fa19289cd706909700cc2
v123582 <v123582@gmail.com>
2018-11-26 00:27:57 +0800

    attach associations to user and task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,23 +1,27 @@
 const db = require('../models')
 const Task = db.Task
+const User = db.User
 
 let taskController = {
   getTasks: (req, res) => {
-    Task.findAll().then((tasks) => {
-      if (req.params.id){
-        Task.findByPk(req.params.id).then(task => {
-          return res.render('tasks', {tasks: tasks, task: task})
-        })
-      } else {
-        return res.render('tasks', {tasks: tasks, task: false})
-      }
-    })
+    return User.findByPk(req.user.id, {include: [Task]})
+      .then(function (user) {
+        if (req.params.id){
+          Task.findByPk(req.params.id).then(task => {
+            return res.render('tasks', {tasks: user.Tasks, task: task})
+          })
+        }
+        else 
+          return res.render('tasks', {tasks: user.Tasks, task: false})
+      });
   },
   postTask: (req, res) => {
     return Task.create({
-      name: req.body.name, 
+      name: req.body.name,
+      done: false,
       createdAt : new Date(),
       updatedAt : new Date(),
+      UserId: req.user.id,
      })
      .then(function (task) {
        res.redirect('/tasks')
--- models/task.js
+++ models/task.js
@@ -2,10 +2,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
     name: DataTypes.STRING,
-    done: DataTypes.BOOLEAN
+    done: DataTypes.BOOLEAN,
   }, {});
   Task.associate = function(models) {
     // associations can be defined here
+    Task.belongsTo(models.User);
   };
   return Task;
 };
