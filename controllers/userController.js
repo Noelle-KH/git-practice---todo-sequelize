29696f7289ef88c740d014655131b0d1116376f3
Noelle-KH <noelletang86@gmail.com>
2023-03-03 11:12:59 +0800

    noelle update

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,9 @@ const User = db.User
 
 let userController = {
   getUsers: (req, res) => {
-    return User.findAll().then((users) => res.json(users))
+    return User.findAll()
+      .then((users) => res.json(users))
+      .catch(error => console.log(error))
   }
 }
 module.exports = userController
