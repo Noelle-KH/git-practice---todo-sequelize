731ff3cacc22f0a80d518280669c4160528651dc
Lilynews <lilinews2021@gmail.com>
2023-03-13 22:54:12 -0400

    feat: 0314 Lily04-2 usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,9 +13,14 @@ let userController = {
   },
   // Lily04 update
   getTopUsers: (req, res) => {
+    // Lily04 update-2
+    const controller = req.userController
+    const data = req.User
+
     return User.findAll()
       .then((users) => res.json(users))
       .catch(error => console.log(error))
   }
 }
+
 module.exports = userController
